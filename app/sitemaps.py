3918af76ab8e70eaee841638344e8405633206e1
012balaji <144882695+012balaji@users.noreply.github.com>
2026-07-28 10:39:04 +0530

    Added fil all

--- app/sitemaps.py
+++ app/sitemaps.py
@@ -7,7 +7,7 @@ class StaticViewSitemap(Sitemap):
 
     def items(self):
         return [
-            '',
+            'loading',
             'home',
             'destination',
             'gallery',
